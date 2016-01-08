2e1efda234e9739c53fd9fa12a4f67408fbe4bcf
jasmis <jonathan.asmis@gmail.com>
2016-01-07 21:42:42 -0800

    Put loan first as per campaign strategy

--- src/components/ServicesPage/index.js
+++ src/components/ServicesPage/index.js
@@ -21,7 +21,7 @@ export class ServicesPage extends Component {
   render() {
     const intro = {
       title: 'Comprehensive support to take the hassle out of getting investment help',
-      text: "We all need a little help buying our first home but it's not always clear how to get it. <br /><br />Landed helps you create and register legal documents, helps track and transfer payments, and acts as an independent arbiter.<br /><br />Whether structured as a lease-to-own agreement, a co-investment, or just a simple loan, Landed is here to help.",
+      text: "We all need a little help buying our first home but it's not always clear how to get it. <br /><br />Landed helps you create and register legal documents, helps track and transfer payments, and acts as an independent arbiter.<br /><br />Whether structured as a loan, a lease-to-own agreement, or a co-investment, Landed is here to help.",
       image: 'services',
     };
 
@@ -40,22 +40,22 @@ export class ServicesPage extends Component {
                 <div className="hidden-md hidden-lg sign-up-block">
                   <Link to="join">
                     <span className="button">
-                      I’m ready for home
+                      Help me with Home
                     </span>
                   </Link>
                 </div>
                 <div className="hidden-xs hidden-sm sign-up-block">
                   <span className="button" onClick={ () => this.props.showPopup(true) }>
-                    I’m ready for home
+                    Help me with Home
                   </span>
                 </div>
               </div>
             </div>
           </div>
           <HowItWorks />
+          <Loan />
           <Lease />
           <Ownership />
-          <Loan />
         </div>
         <ImageBlock { ...imageBlockProps } />
         
