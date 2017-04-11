ae6b8e8aeea23248f62cfab2afaa8fe6dbd990f7
jasmis <jonathan.asmis@gmail.com>
2017-04-11 09:07:36 -0700

    reverting

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import Root from './components/Root';
-
 import About from './components/Pages/About';
 import Calc from './components/Pages/Calc/';
 import Home from './components/Pages/Home';
 import HowItWorks from './components/Pages/HowItWorks';
 import GetLanded from './components/Pages/GetLanded';
-import Invest from './components/Pages/Invest';
-import LosAltosSchoolDistrict from './components/Pages/LosAltosSchoolDistrict';
 
 export default (
   <Route path="/" component={Root}>
@@ -20,8 +17,6 @@ export default (
     <Route path="/calculator" component={Calc} />
     <Route path="/how-it-works" component={HowItWorks} />
     <Route path="/get-landed" component={GetLanded} />
-    <Route path="/invest" component={Invest} />
-    <Route path="/los-altos-school-district" component={LosAltosSchoolDistrict} />
 
     <Route status={404} path="*" component={Home} />
   </Route>
